68d670972c2f2733d80ca041d3d7d9f4cf19fe9f
CLg003 <clairemjlaing@gmail.com>
2022-04-04 15:08:26 +0100

    Extension complete.

--- src/App.js
+++ src/App.js
@@ -3,43 +3,61 @@ import './App.css';
 
 function App() {
 
-  const [tasks, setTasks] = useState(["Wash dishes", "Sweep floor", "Mow lawn"]);
+  const [tasks, setTasks] = useState([
+    {name: "Wash dishes", priority: "high"}, 
+    {name: "Sweep floor", priority: "low"}, 
+    {name: "Mow lawn", priority: "high"}
+  ]);
 
-  const [newTask, setNewTask] = useState('');
+  const [newTaskName, setNewTaskName] = useState('');
+
+  const [newTaskPriority, setNewTaskPriority] = useState('');
 
   const handleTaskInput = (event) => {
-    setNewTask(event.target.value);
+    setNewTaskName(event.target.value);
+  }
+
+  const handlePriorityInput = (event) => {
+    setNewTaskPriority(event.target.value);
   }
 
   const saveNewTask = (event) => {
     event.preventDefault();
     const copyTasks = [...tasks];
-    copyTasks.push(newTask);
+    copyTasks.push({name: newTaskName, priority: newTaskPriority});
     setTasks(copyTasks);
-    setNewTask('');
+    setNewTaskName('');
+    setNewTaskPriority('');
   }
 
   const taskNodes = tasks.map((task, index) => {
     return(
-      <li key={index}><span>{task}</span></li>
+      <li key={index}>
+        <span className={task.priority==="high" ? "high" : "low"}>{task.priority.toUpperCase()} PRIORITY</span>
+        <span>{task.name}</span>
+        </li>
     )
   })
 
   return (
     <div className="app">
 
-      <h1>To Do:</h1>
+      <h1>To Do List:</h1>
       <hr></hr>
 
       <form onSubmit={saveNewTask}>
         <label htmlFor="new-task">Add a new task: </label>
-        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput}/>
-        {/* <label htmlFor="low-priority">Low Priority</label>
-        <input type="radio" name="priority" value="Low"/>
-        <label htmlFor="high-priority">High Priority</label>
-        <input type="radio" name="priority" value="High"/> */}
+        <input id="new-task" type="text" value={newTaskName} onChange={handleTaskInput}/>
+        <div className="radio" onChange={handlePriorityInput}>
+          <label htmlFor="low-priority">Low Priority</label>
+          <input type="radio" name="priority" value="low" checked={newTaskPriority==="low"}/>
+          <label htmlFor="high-priority">High Priority</label>
+          <input type="radio" name="priority" value="high" checked={newTaskPriority==="high"}/>
+        </div>
         <button type="submit">Add Task</button>
       </form>
+
+      <h2>Tasks</h2>
 
       <ul>
         {taskNodes}
@@ -47,5 +65,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
